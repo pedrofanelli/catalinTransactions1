5d42cbd317d924b467e3b7377822fdd334d83a8a
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-26 15:34:36 -0300

    forming pom

--- src/test/java/com/example/demo/catalinTransactions1/AppTest.java
+++ src/test/java/com/example/demo/catalinTransactions1/AppTest.java
@@ -1,8 +1,8 @@
 package com.example.demo.catalinTransactions1;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
