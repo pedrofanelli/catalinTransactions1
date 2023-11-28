0b90b6a303e358571246fa6eaff2b4761dd61cb5
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-11-28 19:52:15 -0300

    testdata

---
+++ src/test/java/com/example/demo/catalinTransactions1/TestData.java
@@ -0,0 +1,17 @@
+package com.example.demo.catalinTransactions1;
+
+public class TestData {
+	public final Long[] identifiers;
+
+    public TestData(Long[] identifiers) {
+        this.identifiers = identifiers;
+    }
+
+    public Long getFirstId() {
+        return identifiers.length > 0 ? identifiers[0] : null;
+    }
+
+    public Long getLastId() {
+        return identifiers.length > 0 ? identifiers[identifiers.length - 1] : null;
+    }
+}
